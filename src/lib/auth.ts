507fca94bf0049e11729c68386f976402bacb534
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-08-19 18:34:45 +0600

    fixing the authentication error related to upstash

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -2,6 +2,7 @@ import { NextAuthOptions } from "next-auth";
 import { db } from "./db";
 import { UpstashRedisAdapter } from "@next-auth/upstash-redis-adapter";
 import GoogleProvider from "next-auth/providers/google";
+import { fetchRedis } from "@/helpers/redis";
 
 function getGoogleCredentials() {
   const clientId = process.env.GOOGLE_CLIENT_ID;
@@ -34,22 +35,26 @@ export const authOptions: NextAuthOptions = {
   ],
   callbacks: {
     async jwt({ token, user }) {
-      if (user) {
-        const dbUser = (await db.get(`user: ${token.id}`)) as User | null;
+      const dbUserResult = (await fetchRedis("get", `user:${token.id}`)) as
+        | string
+        | null;
 
-        if (dbUser) {
-          return {
-            id: dbUser.id,
-            name: dbUser.name,
-            email: dbUser.email,
-            image: dbUser.image,
-          };
-        } else {
-          token.id = user.id;
+      if (!dbUserResult) {
+        if (user) {
+          token.id = user!.id;
         }
+
+        return token;
       }
 
-      return token;
+      const dbUser = JSON.parse(dbUserResult) as User;
+
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      };
     },
 
     async session({ session, token }) {
