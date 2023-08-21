735a4cb82ea8e940b0bcc5764ca809fb2d15a3fc
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-08-21 22:41:49 +0600

    the initial chat list page

---
+++ src/app/dashboard/chat/[chatId]/page.tsx
@@ -0,0 +1,62 @@
+import { fetchRedis } from "@/helpers/redis";
+import { authOptions } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { messageArrayValidator } from "@/lib/validations/message";
+import { getServerSession } from "next-auth";
+import { notFound } from "next/navigation";
+import { FC } from "react";
+
+interface pageProps {
+  params: {
+    chatId: string;
+  };
+}
+
+async function getChatMessages(chatId: string) {
+  try {
+    const results: string[] = await fetchRedis(
+      "zrange",
+      `chat:${chatId}:messages`,
+      0,
+      -1
+    );
+
+    const dbMessages = results.map((message) => JSON.parse(message) as Message);
+
+    const reversedDbMessages = dbMessages.reverse();
+
+    const messages = messageArrayValidator.parse(reversedDbMessages);
+
+    return messages;
+  } catch (error) {
+    notFound();
+  }
+}
+
+const page: FC<pageProps> = async ({ params }: pageProps) => {
+  const { chatId } = params;
+  const session = await getServerSession(authOptions);
+
+  if (!session) notFound();
+
+  const { user } = session;
+
+  const [userId1, userId2] = chatId.split("--");
+
+  //if none of the user id is yours, you can not view the chat
+  if (user.id !== userId1 && user.id !== userId2) {
+    notFound();
+  }
+
+  //to determine which of the chat id is your chat partner's
+  const chatPartnerId = user.id === userId1 ? userId2 : userId1;
+
+  //getting chatPartners info
+  const chatPartner = (await db.get(`user:${chatPartnerId}`)) as User;
+
+  const initialMessages = await getChatMessages(chatId);
+
+  return <div>{chatId}</div>;
+};
+
+export default page;
