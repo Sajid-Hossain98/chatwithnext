704880815c38757f02eb70d704556c36d45c6aab
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-08-25 19:01:29 +0600

    realtime friend request(server setup)

--- src/app/api/friends/add/route.ts
+++ src/app/api/friends/add/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { addFriendValidator } from "@/lib/validations/add-friend";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
@@ -43,14 +45,14 @@ export async function POST(req: Request) {
     }
 
     //sent a request already
-    const isAlreadyAdded = (await fetchRedis(
+    const isAlreadySentRequest = (await fetchRedis(
       "sismember",
-      `user:${idToAdd}:incoming_friend_request`,
+      `user:${idToAdd}:incoming_friend_requests`,
       session.user.id
     )) as 0 | 1;
 
-    if (isAlreadyAdded) {
-      return new Response("Already added this user", {
+    if (isAlreadySentRequest) {
+      return new Response("Already sent a request to this user", {
         status: 400,
       });
     }
@@ -68,6 +70,15 @@ export async function POST(req: Request) {
     }
 
     //valid request, send friend request
+    pusherServer.trigger(
+      toPusherKey(`user:${idToAdd}:incoming_friend_requests`),
+      "incoming_friend_requests",
+      {
+        senderId: session.user.id,
+        senderEmail: session.user.email,
+      }
+    );
+
     db.sadd(`user:${idToAdd}:incoming_friend_requests`, session.user.id);
 
     return new Response("OK");
