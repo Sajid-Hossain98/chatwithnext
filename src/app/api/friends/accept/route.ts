740f4a402bdca83e18760c260c366f37f2913286
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-08-28 14:38:54 +0600

    listen for friend request, if added then refresh

--- src/app/api/friends/accept/route.ts
+++ src/app/api/friends/accept/route.ts
@@ -1,6 +1,8 @@
 import { fetchRedis } from "@/helpers/redis";
 import { authOptions } from "@/lib/auth";
 import { db } from "@/lib/db";
+import { pusherServer } from "@/lib/pusher";
+import { toPusherKey } from "@/lib/utils";
 import { getServerSession } from "next-auth";
 import { z } from "zod";
 
@@ -42,6 +44,13 @@ export async function POST(req: Request) {
         status: 400,
       });
     }
+
+    //notify user who sent the request
+    pusherServer.trigger(
+      toPusherKey(`user:${idToAdd}:friends`),
+      "new_friend",
+      {}
+    );
 
     //to add the user who sent the request to the user's friends list who is accepting the request
     await db.sadd(`user:${session.user.id}:friends`, idToAdd);
