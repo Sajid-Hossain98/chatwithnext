93696252c7822b9fb380b7b9cd00b89118f6274c
Sajid Hossain <bmsajidhossain420@gmail.com>
2023-08-29 21:17:15 +0600

    build update for next.config.js

--- next.config.js
+++ next.config.js
@@ -3,12 +3,12 @@ const nextConfig = {
   images: {
     domains: ["lh3.googleusercontent.com", "i.ibb.co"],
   },
-  // typescript: {
-  //   ignoreBuildErrors: true,
-  // },
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
 };
 
 module.exports = nextConfig;
